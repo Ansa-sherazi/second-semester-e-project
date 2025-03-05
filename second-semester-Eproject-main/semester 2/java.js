bfb92c5179335efcc81f840d5ed4e7ba0419f11e
Ansa-sherazi <155802749+Ansa-sherazi@users.noreply.github.com>
2025-03-05 16:20:06 +0500

    added

---
+++ second-semester-Eproject-main/semester 2/java.js
@@ -0,0 +1,45 @@
+
+// signup
+
+function handleEvent(event){
+	event.preventDefault();
+	alert('Your Form Submitted!')
+  }
+
+
+
+
+
+
+// login
+
+
+  function handleEvent(event){
+	event.preventDefault();
+	alert('Your Form Submitted!')
+  }
+
+
+
+
+
+// forgetpass
+
+function handleEvent(event){
+	event.preventDefault();
+	alert('Your Form Submitted!')
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
